36d4c767a8f41fb4c31358b8288ede013dcde547
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-05-16 13:34:34 -0700

    Add prevent form submit when invalid input.

--- question3/js/scripts.js
+++ question3/js/scripts.js
@@ -14,12 +14,20 @@ $(document).ready(function () {
             artist: {
               required: true,
             }
-
-        },
-        submitHandler: function (form) { // for demo
-            alert('valid form submitted'); // for demo
-            return false; // for demo
         }
     });
+
+    $('#myForm').submit(function(event){
+    var isvalidate = $("#myForm").valid();
+            if(!isvalidate)
+            {
+                event.preventDefault();
+                alert("One or more invalid inputs.");
+            } else {
+               e.preventDefault();
+              //Do AJAX
+    }
+
+         });
 
 });
